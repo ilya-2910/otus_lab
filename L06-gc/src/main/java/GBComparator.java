9da4c42020a6b32c4de1fdb63ad526f394eb6d88
ilya-2910 <nasipov@gmail.com>
2019-12-02 22:10:13 +0300

    #L06-gc initial commit

---
+++ L06-gc/src/main/java/GBComparator.java
@@ -0,0 +1,60 @@
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+import javax.management.openmbean.CompositeData;
+import java.lang.management.GarbageCollectorMXBean;
+import java.util.List;
+
+public class GBComparator {
+
+    public static void main(String[] args) throws Exception {
+        switchOnMonitoring();
+        generateOOM();
+    }
+
+    public static void generateOOM() throws Exception {
+        int iteratorValue = 2000;
+        for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
+            System.out.println("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
+            int loop1 = 2;
+            int[] memoryFillIntVar = new int[iteratorValue];
+            // feel memoryFillIntVar array in loop..
+            do {
+                memoryFillIntVar[loop1] = 0;
+                loop1--;
+            } while (loop1 > 0);
+            iteratorValue = iteratorValue * 2;
+            System.out.println("Required Memory for next loop: " + iteratorValue);
+            Thread.sleep(1000);
+        }
+    }
+
+    private static void switchOnMonitoring() {
+        List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcbean : gcbeans) {
+            System.out.println("GC name:" + gcbean.getName());
+            NotificationEmitter emitter = (NotificationEmitter) gcbean;
+            NotificationListener listener = (notification, handback) -> {
+                if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+                    String gcName = info.getGcName();
+                    String gcAction = info.getGcAction();
+                    String gcCause = info.getGcCause();
+
+                    long startTime = info.getGcInfo().getStartTime();
+                    long duration = info.getGcInfo().getDuration();
+
+                    System.out.println("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
+                }
+            };
+            emitter.addNotificationListener(listener, null, null);
+        }
+    }
+
+    class GcInfo {
+        int count;
+        int duration;
+    }
+
+}
