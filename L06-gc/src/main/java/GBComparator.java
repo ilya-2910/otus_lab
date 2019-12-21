defbc1be83a991401faad6ceb700ac21ca3a5ea6
ilya-2910 <nasipov@gmail.com>
2019-12-22 02:34:36 +0300

    #L06-gc доработка

--- L06-gc/src/main/java/GBComparator.java
+++ L06-gc/src/main/java/GBComparator.java
@@ -4,33 +4,67 @@ import javax.management.NotificationEmitter;
 import javax.management.NotificationListener;
 import javax.management.openmbean.CompositeData;
 import java.lang.management.GarbageCollectorMXBean;
+import java.util.Date;
 import java.util.List;
 
 public class GBComparator {
 
     public static void main(String[] args) throws Exception {
-        switchOnMonitoring();
-        generateOOM();
+        new GBComparator().start();
     }
 
-    public static void generateOOM() throws Exception {
-        int iteratorValue = 2000;
-        for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
-            System.out.println("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
-            int loop1 = 2;
-            int[] memoryFillIntVar = new int[iteratorValue];
-            // feel memoryFillIntVar array in loop..
-            do {
-                memoryFillIntVar[loop1] = 0;
-                loop1--;
-            } while (loop1 > 0);
-            iteratorValue = iteratorValue * 2;
-            System.out.println("Required Memory for next loop: " + iteratorValue);
-            Thread.sleep(1000);
+    private GcInfo youngGcInfo;
+    private GcInfo oldGcInfo;
+
+    private static String G1 = "G1";
+    private static String PARALLEL = "PARALLEL GC";
+
+    public void start() {
+        Date startDate = new Date();
+        // "G1" or "parralel"
+        switchOnMonitoring(G1);
+        try {
+            generateOOM();
+        } catch (Throwable e) {
+            Date endDate = new Date();
+            System.out.println("execution time sec=" + (endDate.getTime() - startDate.getTime()) / 1000);
+
+            printInfo(youngGcInfo);
+            printInfo(oldGcInfo);
         }
     }
 
-    private static void switchOnMonitoring() {
+    private void printInfo(GcInfo gcInfo) {
+        System.out.println("******" + gcInfo.getName() + "*******");
+        System.out.println("all gc pause (throughput) ms=" + (gcInfo.getDuration()));
+        System.out.println("count gc pause=" + (gcInfo.getCount()));
+        System.out.println("maxPause (latency)=" + (gcInfo.getMaxPause()));
+    }
+
+    public void generateOOM() throws Exception {
+        int arraySize = 0;
+        for (int i = 1; i < 100000; i++) {
+            System.out.println("Iteration " + i + " Free Mem: " + Runtime.getRuntime().freeMemory());
+            int[] memoryFillIntVar = new int[arraySize];
+            for (int j = 0; j < arraySize; j++) {
+                memoryFillIntVar[j] = 0;
+            }
+            arraySize = arraySize + 10000;
+            System.out.println("Required Memory for next loop: " + arraySize);
+            Thread.sleep(500);
+        }
+    }
+
+    private void switchOnMonitoring(String type) {
+        if (type == G1) {
+            youngGcInfo = new GcInfo("Young Generation");
+            oldGcInfo = new GcInfo("Old Generation");
+        } else if (type == PARALLEL) {
+            youngGcInfo = new GcInfo("PS Scavenge");
+            oldGcInfo = new GcInfo("PS MarkSweep");
+        }
+
+
         List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
         for (GarbageCollectorMXBean gcbean : gcbeans) {
             System.out.println("GC name:" + gcbean.getName());
@@ -46,6 +80,18 @@ public class GBComparator {
                     long duration = info.getGcInfo().getDuration();
 
                     System.out.println("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
+                    GcInfo gcInfo = null;
+                    if (gcName.contains(oldGcInfo.getName())) {
+                        gcInfo = oldGcInfo;
+                    } else if (gcName.contains(youngGcInfo.getName())) {
+                        gcInfo = youngGcInfo;
+                    }
+
+                    gcInfo.setCount(gcInfo.getCount() + 1);
+                    gcInfo.setDuration(gcInfo.getDuration() + duration);
+                    if (duration > gcInfo.getMaxPause()) {
+                        gcInfo.setMaxPause(duration);
+                    }
                 }
             };
             emitter.addNotificationListener(listener, null, null);
@@ -53,8 +99,42 @@ public class GBComparator {
     }
 
     class GcInfo {
+        private final String name;
         int count;
-        int duration;
+        long duration;
+        long maxPause;
+
+        public GcInfo(String name) {
+            this.name = name;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
+        }
+
+        public long getDuration() {
+            return duration;
+        }
+
+        public void setDuration(long duration) {
+            this.duration = duration;
+        }
+
+        public long getMaxPause() {
+            return maxPause;
+        }
+
+        public void setMaxPause(long maxPause) {
+            this.maxPause = maxPause;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 
 }
