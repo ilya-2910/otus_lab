e06fa800c187a3e89277a9f633571c8c0eea0ac2
ilya-2910 <ilja.nasipov@wisercat.eu>
2020-05-10 15:38:15 +0300

    # initial commit

---
+++ hw-15-jmm/src/main/java/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Main counter = new Main();
+        counter.go();
+    }
+
+    private String last = "thread_1";
+
+    private synchronized void print(String thread) {
+        for (int i = 0; i <= 20; i++) {
+            int j = i;
+            if (i > 10) {
+                j = 20 - i;
+            }
+            try {
+                while (last.equals(thread)) {
+                    this.wait();
+                }
+                System.out.println(j);
+                last = thread;
+                notifyAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void go() {
+        Thread thread1 = new Thread(() -> print("thread_1"));
+        Thread thread2 = new Thread(() -> print("thread_2"));
+        thread1.start();
+        thread2.start();
+    }
+
+}
